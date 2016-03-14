89687d5e401d3fcfd9cee84d99ff8110eaaa4a6c
Samuel B Lyon <Sam@Samuels-MacBook-Air.local>
2016-03-14 15:36:31 -0700

    completed text scrape module

--- wcp.py
+++ wcp.py
@@ -1,17 +1,13 @@
 import sys
 import requests
-import BeautifulSoup
+# import BeautifulSoup
 import getopt
 from lxml import html
 
 url = sys.argv[1]
-
-print "pinging " + url
-
 page = requests.get(url)
+tree = html.fromstring(page.content)
+text_array = tree.xpath('//div[@id="mw-content-text"]/p//text()[not(ancestor::sup)]')
+text = "".join(text_array)
+print text.encode("utf-8")
 
-print page
-
-stuff = html.fromstring(page.content)
-
-print stuff
