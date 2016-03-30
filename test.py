5965b67a7f32e71c58a2811bb7d9b64b9f524879
Nick Lisi <nick@prudwell.com>
2016-03-30 10:38:43 -0700

    working on stripping html tags

--- test.py
+++ test.py
@@ -1,11 +1,18 @@
 import requests
-import BeautifulSoup
+from BeautifulSoup import BeautifulSoup
 import sys
-import getopt
-
-print sys.argv[1]
-
-url = sys.argv[1]
-page = requests.get(url)
-
-print page.content
+import argparse
+parser = argparse.ArgumentParser(description='Count Words')
+parser.add_argument('url', metavar='URL', type=str,
+                    help='a URL')
+args = parser.parse_args()
+print args.url
+response = requests.get(args.url)
+soup = BeautifulSoup(response.content)
+content = soup.find("div", { "id" : "mw-content-text" })
+content_text = content.getText()
+content_text_string = content_text.encode('ascii', 'replace')
+#content_text = repr(content_text) #convert the unicode to a string 
+f = open('workfile', 'r+')
+f.write(content_text_string)
+print (content_text_string)
