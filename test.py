e41cb7d6d6dd50dd3feeb55ad38dbcca7d5dcf58
Nick Lisi <nick@prudwell.com>
2016-03-30 10:47:51 -0700

    added comments and change r+ to w+ for file opening so that it creates the file if it does not exit

--- test.py
+++ test.py
@@ -7,12 +7,27 @@ parser.add_argument('url', metavar='URL', type=str,
                     help='a URL')
 args = parser.parse_args()
 print args.url
+
+#make the http request for the URL we supplied
 response = requests.get(args.url)
+
+#instantiate a beautifulsoup parser object using the content from the request we just made
 soup = BeautifulSoup(response.content)
+
+#look for the mw-content-text div on a wikipedia page
 content = soup.find("div", { "id" : "mw-content-text" })
+
+#getText the content object to get just the text without tags
 content_text = content.getText()
+
+#getText returns a unicode encoded string which is difficult to work with, I was getting errors. attempting to encode it to ascii
 content_text_string = content_text.encode('ascii', 'replace')
-#content_text = repr(content_text) #convert the unicode to a string 
-f = open('workfile', 'r+')
+
+#content_text_string = repr(content_text) #another method I was trying for encoding
+
+#write the output to a file called workfile so we can look at the entire output 
+f = open('workfile', 'w+')
 f.write(content_text_string)
+
+#also print to terminal
 print (content_text_string)
